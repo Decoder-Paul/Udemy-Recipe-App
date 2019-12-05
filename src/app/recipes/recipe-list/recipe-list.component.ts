fd97796d3443d488b125541941c0d3e598dde840
Subhankar Paul <paul93subhankar@gmail.com>
2019-12-06 00:35:33 +0530

    Till lecture 59|Section 3 closed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'A simple test recipe details', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
     new Recipe('Test Recipe', 'A simple test recipe details', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
   constructor() { }
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,9 +1,9 @@
-export class Recipe{
+export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
 
-  constructor(name: string, description: string, imagepath: string){
+  constructor(name: string, description: string, imagepath: string) {
     this.name = name;
     this.description = description;
     this.imagePath = imagepath;
